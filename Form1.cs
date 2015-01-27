a89f34c251c3b28cf6f83e5553a29d0944a6b39a
ahmad009 <ahmad_125@ymail.com>
2015-01-27 22:08:46 +0330

    Zlib Tool
    Zlib Compressor And Decompressor

---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Ionic.Zlib;
+using System.IO;
+
+namespace ZlibTool
+{
+    public partial class Form1 : Form
+    {
+        string inputpath;
+        string inputfile;
+
+        OpenFileDialog openFileDialog1 = new OpenFileDialog();
+        SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "All Files Compressed With Zlib (*.*)|*.*";
+
+            openFileDialog1.FilterIndex = 1;
+
+            openFileDialog1.Multiselect = false;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                button2.Enabled = true;
+                button3.Enabled = true;
+
+                inputpath = Path.GetDirectoryName(openFileDialog1.FileName);
+                inputfile = Path.GetFileName(openFileDialog1.FileName);
+                
+                comboBox1.Text = inputfile;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "All Files (*.*)|*.*";
+            saveFileDialog1.ShowDialog();
+
+            if (saveFileDialog1.FileName != null)
+            {
+                byte[] bytes = System.IO.File.ReadAllBytes(openFileDialog1.FileName);
+                var compressedBlob = ZlibStream.CompressBuffer(bytes);
+
+                try
+                {
+                    System.IO.FileStream _FileStream = (System.IO.FileStream)saveFileDialog1.OpenFile();
+                    _FileStream.Write(compressedBlob, 0, compressedBlob.Length);
+                    _FileStream.Close();
+                }
+                catch
+                { }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "All Files (*.*)|*.*";
+            saveFileDialog1.ShowDialog();
+
+            if (saveFileDialog1.FileName != null)
+            {
+                byte[] bytes = System.IO.File.ReadAllBytes(openFileDialog1.FileName);
+                var decompressedBlob = ZlibStream.UncompressBuffer(bytes);
+
+                try
+                {
+                    System.IO.FileStream _FileStream = (System.IO.FileStream)saveFileDialog1.OpenFile();
+                    _FileStream.Write(decompressedBlob, 0, decompressedBlob.Length);
+                    _FileStream.Close();
+                }
+                catch
+                { }
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ZlibTool")]
+[assembly: AssemblyDescription("made by Ahmad009")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ZlibTool")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7313c142-4eff-428f-9151-e393a0ed7a35")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
